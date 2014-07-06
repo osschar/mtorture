47991f5c491b3661e57ab2709e3bf2a4f9deb4b8
Matevz Tadel <mtadel@ucsd.edu>
2014-07-05 18:12:16 -0700

    More tests, add script to run a series of tests.

---
+++ Test.pm
@@ -0,0 +1,136 @@
+package Test;
+
+use Carp;
+
+########################################################################
+# Helper functions
+########################################################################
+
+sub exec_or_die
+{
+  # Execute command and return its stdout.
+
+  my $cmd = shift;
+
+  my $ret = `$cmd`;
+  croak "$cmd died" if $?;
+  return $ret
+}
+
+sub system_or_die
+{
+  # Execute command via "system" call.
+
+  my $cmd = shift;
+
+  system "$cmd" and croak "$cmd died";
+}
+
+sub spawn
+{
+  # Fork a new process, redirect output to $log. Retunrs pid of the forked
+  # process.
+
+  my $log = shift;
+  # Rest of arguments passed to exec
+
+  my $pid = fork();
+
+  if ($pid == 0)
+  {
+    open STDOUT, '>', $log  or die "Can't redirect STDOUT: $!";
+    # open STDERR, ">&STDOUT" or die "Can't dup STDOUT: $!";
+
+    select STDERR; $| = 1; # make unbuffered
+    select STDOUT; $| = 1; # make unbuffered
+
+    setpriority(0, 0, $NICE) if $NICE;
+
+    exec @_;
+  }
+
+  return $pid;
+}
+
+sub wait_all
+{
+  # Wait for all child processes to finish.
+
+  {
+    # print "Waiting now ... ";
+    my $pid = wait();
+    my $est = $pid != -1 ? $? >> 8 : 'n/a';
+    # print "pid = $pid, status = $est\n";
+
+    redo if $pid != -1;
+  }
+}
+
+########################################################################
+# Class Test
+########################################################################
+
+sub new
+{
+  my $proto = shift;
+  my $class = ref($proto) || $proto;
+
+  my $S = {@_};
+  bless($S, $class);
+
+  ### For sub classes:
+  ### @ISA = ('Test');
+  ### my $S = $proto->SUPER::new(@_);
+
+  $S->{'ExeEnv'}  = join(' ', map { defined $S->{$_} ? "$_=$S->{$_}" : () }
+                         qw(TEST_DURATION PRE_TEST_FRAC N_VEC_MIN N_VEC_MAX));
+
+  $S->{'Src'}     = "$S->{'Base'}.cxx";
+  $S->{'ExeHost'} = "$S->{'Base'}";
+  $S->{'ExeMic'}  = "$S->{'Base'}-mic";
+  $S->{'AllExes'} = "$S->{'ExeHost'} $S->{'ExeMic'}";
+
+  $S->{'CmdHost'} = "$S->{'ExeEnv'} ./$S->{'ExeHost'}";
+  $S->{'CmdMic'}  = "ssh mic0 $S->{'ExeEnv'} ./$S->{'ExeMic'}";
+
+  return $S;
+}
+
+sub print_vars
+{
+  my $S = shift;
+
+  for $k (sort keys %$S)
+  {
+    printf("%12s   =>   %s\n", $k, $S->{$k});
+  }
+}
+
+########################################################################
+
+sub make_clean
+{
+  my $S = shift;
+
+  system_or_die("make $S->{'MAKE_VARS'} clean");
+}
+
+sub run_test
+{
+  my ($S, $test) = @_;
+
+  # Force recompilation of test executable with appropriate define of TEST_FUNC.
+  system_or_die("make $S->{'MAKE_VARS'} USER_CPPFLAGS=\"-DTEST_FUNC=$test\" -W $S->{'Src'} $S->{'AllExes'}");
+
+  my $ohost = eval "\"$S->{'FmtHost'}\"";
+  my $omic  = eval "\"$S->{'FmtMic'}\"";
+
+  spawn($ohost, $S->{'CmdHost'});
+  spawn($omic,  $S->{'CmdMic'});
+
+  wait_all();
+}
+
+########################################################################
+
+1;
---
+++ t1.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl
+
+use lib ".";
+use Test;
+
+########################################################################
+
+my $t = new Test('Base'          => 't1',
+                 'FmtHost'       => 'xxx_${test}_host.rt',
+                 'FmtMic'        => 'xxx_${test}_mic.rt',
+
+                 # 'MAKE_VARS'     => 'OPT=-O3',
+
+                 # Defaults for test duration and problem-size limits:
+                 # 'TEST_DURATION' => 1,
+                 # 'N_VEC_MIN'     => 8,
+                 # 'N_VEC_MAX'     => 64 * 1024 * 1024,
+
+                 'TEST_DURATION' => 0.2,
+                 'N_VEC_MIN'     => 1024,
+                 'N_VEC_MAX'     => 8 * 1024,
+    );
+
+@TESTS = qw(sum2 sum2_sqr sum2_cube sum2_quad sum2_quint
+            sum3 sum3_sqr sum3_cube);
+
+$t->make_clean();
+
+for $test (@TESTS)
+{
+  print "Running test $test ...\n";
+
+  $t->run_test($test);
+}
