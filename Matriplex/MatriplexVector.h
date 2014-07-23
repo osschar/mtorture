4cbba9bc9a021056b89813dd05778b03da8f1ab1
Matevz Tadel <mtadel@ucsd.edu>
2014-07-23 12:46:27 -0700

    Only call the intrinsic version for 3x3 sym matrices (6x6 was broken for a while because of that).

--- Matriplex/MatriplexVector.h
+++ Matriplex/MatriplexVector.h
@@ -128,7 +128,11 @@ void Multiply(const MPlexVec<MPlexSym<T, D, N>>& A,
    for (int i = 0; i < np; ++i)
    {
 #ifdef __MIC__
-      Multiply3x3SymIntrinsic(A[i], B[i], C[i]);
+      // XXXX Need to do 6x6 intrinsic multiplication.
+      if(D == 3)
+         Multiply3x3SymIntrinsic(A[i], B[i], C[i]);
+      else
+         Multiply(A[i], B[i], C[i]);
 #else
       Multiply(A[i], B[i], C[i]);
 #endif
