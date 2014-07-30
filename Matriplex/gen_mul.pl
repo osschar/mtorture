f494c81d9c4409c2731bc4fe104b58d2c9af1573
Matevz Tadel <mtadel@ucsd.edu>
2014-07-29 23:18:22 -0700

    * Cut into subroutines. * Add code to generate symetric multiplication with intrinsics. * Function to be executed has to be passed in as a command-line argument.

--- Matriplex/gen_mul.pl
+++ Matriplex/gen_mul.pl
@@ -10,11 +10,6 @@ $BR   = " ";
 $JOIN = "$BR";
 $POST = " ";
 
-$D   = 3;
-@Off = @{$Offs[$D]};
-
-$E = $D;
-# $E = 3; # for Kalman gain multiply
 
 #$a = "A.fArray";
 #$b = "B.fArray";
@@ -23,10 +18,18 @@ $a = "a";
 $b = "b";
 $c = "c";
 
+$vectype = "__m512";
+
 $SYMMETRIC = 1;
 
-if ($SYMMETRIC)
-{ 
+################################################################################
+
+sub mult_sym
+{
+  my $D = shift;
+
+  my @Off = @{$Offs[$D]};
+
   for (my $i = 0; $i < $D; ++$i)
   {
     for (my $j = 0; $j < $D; ++$j)
@@ -38,7 +41,7 @@ if ($SYMMETRIC)
 
       my @sum;
 
-      for (my $k = 0; $k < $E; ++$k)
+      for (my $k = 0; $k < $D; ++$k)
       {
         my $iko = $Off[$i * $D + $k];
         my $kjo = $Off[$k * $D + $j];
@@ -49,10 +52,154 @@ if ($SYMMETRIC)
       print "\n";
     }
   }
+}
+
+sub mult_sym_fma
+{
+  # This actually runs quite horribly, twice slower than the
+  # fully expressd version.
+  # Changing order of k and i loops doesn't change anything.
+  #
+  # However, this should be close to what we need for auto-generated
+  # intrinsics!
+
+  my $D = shift;
+
+  my @Off = @{$Offs[$D]};
+
+  for (my $i = 0; $i < $D; ++$i)
+  {
+    for (my $k = 0; $k < $D; ++$k)
+    {
+      for (my $j = 0; $j < $D; ++$j)
+      {
+        my $x = $i * $D + $j;
+        my $iko = $Off[$i * $D + $k];
+        my $kjo = $Off[$k * $D + $j];
+
+        my $op = ($k == 0) ? " =" : "+=";
+
+        printf "${PREF}${c}[%2d*N+n] ${op} ${a}[%2d*N+n]*${b}[%2d*N+n];\n",
+           $x, $iko, $kjo;
+      }
+      print "\n";
+    }
+  }
+}
+
+sub reg_name
+{
+  my ($var, $idx) = @_;
+
+  return "${var}_${idx}";
 
 }
-else
+
+sub load_if_needed
 {
+  my ($var, $idx, $arc) = @_;
+
+  my $reg = reg_name(${var}, ${idx});
+
+  if ($arc->[$idx] == 0)
+  {
+    print "${PREF}${vectype} ${reg} = LD($var, $idx);\n";
+    ++$tick;
+  }
+
+  ++$arc->[$idx];
+
+  return $reg;
+}
+
+sub store
+{
+  my ($var, $idx) = @_;
+
+  my $reg = reg_name(${var}, ${idx});
+
+  print "${PREF}ST(${var}, ${idx}, ${reg});\n";
+
+  return $reg;
+}
+
+sub mult_sym_fma_intrinsic
+{
+  # Order of k and i loops should be different for 3x3 and 6x6. (?)
+
+  my $D = shift;
+
+  my @Off = @{$Offs[$D]};
+
+  local $tick = 0;
+
+  # Counts of use. For a and b to fetch, for c to store
+  my @ac, @bc, @cc, @to_store;
+
+  for (my $i = 0; $i < $D; ++$i)
+  {
+    for (my $k = 0; $k < $D; ++$k)
+    {
+      for (my $j = 0; $j < $D; ++$j)
+      {
+        my $x = $i * $D + $j;
+        my $iko = $Off[$i * $D + $k];
+        my $kjo = $Off[$k * $D + $j];
+
+        my $areg = load_if_needed("a", $iko, \@ac);
+        my $breg = load_if_needed("b", $kjo, \@bc);
+        my $creg = reg_name("c", $x);
+
+        my $op = ($k == 0) ? "=" : "+=";
+
+        if ($k == 0)
+        {
+          print "${PREF}${vectype} ${creg} = MUL(${areg}, ${breg});\n";
+        }
+        else
+        {
+          print "${PREF}${creg} = FMA(${areg}, ${breg}, ${creg});\n";
+        }
+
+        ++$tick;
+
+        if ($k + 1 == $D)
+        {
+          $cc[$x] = $tick + 4; #### Will be ready to store in 4 cycles. Really 4?
+          push @to_store, $x;
+        }
+
+        # Try to store the finished ones.
+        while (1)
+        {
+          last unless @to_store;
+          my $s = $to_store[0];
+          last if $tick < $cc[$s];
+
+          store("c", $s);
+          shift @to_store;
+          ++$tick;
+        }
+
+      }
+      print "\n";
+    }
+  }
+
+  for $s (@to_store)
+  {
+    store("c", $s);
+
+    ++$tick;
+  }
+}
+
+################################################################################
+
+sub mult_std
+{
+  my $D = shift;
+
   for (my $i = 0; $i < $D; ++$i)
   {
     for (my $j = 0; $j < $D; ++$j)
@@ -73,5 +220,23 @@ else
       print "\n";
     }
   }
-
 }
+
+################################################################################
+
+if (scalar @ARGV != 1)
+{
+  print STDERR "Usage: $0 function_call\n";
+  print STDERR << "FNORD";
+Some options:
+  $0 "mult_sym(3);"
+  $0 "mult_sym_fma(3);"
+  $0 "mult_sym_fma_intrinsic(6);"
+
+  $0 "mult_std();"
+FNORD
+
+  exit(1);
+}
+
+eval $ARGV[0];
