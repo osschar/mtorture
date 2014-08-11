6b496cbe50b3adb056ae65f8e2466e02c4f1be40
Matevz Tadel <mtadel@ucsd.edu>
2014-08-11 08:36:21 -0700

    Add test that measures 'absolute' performance versus number of tracks (using Timing).

--- mkFit/main.cc
+++ mkFit/main.cc
@@ -2,16 +2,99 @@
 
 #include "fittest.h"
 
-int main()
+#include "MkFitter.h"
+
+#include "Timing.h"
+
+#include <limits>
+
+std::vector<Track> simtracks;
+
+std::vector<Track> smat_tracks;
+std::vector<Track> plex_tracks;
+
+//==============================================================================
+
+MkFitter *mkfp;
+
+const int Nhits = 10; // XXXXX ARGH !!!! What if there's a missing / double layer?
+
+const int Nloop = 1;
+
+long64 single_run(int n_tracks)
 {
-  int  Ntracks  = 5000;
+  int theEnd = n_tracks;
+
+  for (int itrack = 0; itrack < theEnd; itrack += NN)
+  {
+    int end = std::min(itrack + NN, theEnd);
+
+    mkfp->InputTracksAndHits(simtracks, itrack, end);
+
+    for (int x = 0; x < Nloop; ++x)
+    {
+      if (x != 0)
+      {
+        mkfp->InputTracksOnly(simtracks, itrack, end);
+      }
+
+      mkfp->FitTracks();
+    }
+
+#ifndef NO_ROOT
+    mkfp->OutputFittedTracks(rectracks, itrack, end);
+#endif
+  }
+
+  return long64(Nloop) * n_tracks * 374 * Nhits;
+}
+
+void test_matriplex()
+{
+  int Nmin  = 16;
+  int Nmax  = 32*1024; //32 * 1024;
+
+  generateTracks(simtracks, Nmax);
+
+  mkfp = new (_mm_malloc(sizeof(MkFitter), 64)) MkFitter(Nhits);
+
+  Timing t([&](int n_vec)
+           {
+             return single_run(n_vec);
+           });
+
+  t.print_tuple_header();
+
+  // Warm up
+  t.time_loop(4096, 4);
+
+  for (int n = Nmin; n <= Nmax; n *= 2)
+  {
+    // Nloop = std::numeric_limits<int>::max() / (374 * Nhits * n);
+
+    // printf("XXX n=%d, nloop=%d\n", n, Nloop);
+
+    // t.auto_time_loop(n, 2);
+    
+    t.time_loop(n, 1);
+
+    t.print(n);
+  }
+
+  _mm_free(mkfp);
+}
+
+//------------------------------------------------------------------------------
+
+void test_standard()
+{
+  int  Ntracks  = 16 * 1024;
   bool saveTree = false;
 
-  std::vector<Track> simtracks;
   generateTracks(simtracks, Ntracks);
 
-  std::vector<Track> smat_tracks; smat_tracks.reserve(simtracks.size());
-  std::vector<Track> plex_tracks; plex_tracks.resize (simtracks.size());
+  smat_tracks.reserve(simtracks.size());
+  plex_tracks.resize (simtracks.size());
 
 
   double tmp, tsm;
@@ -26,6 +109,15 @@ int main()
   make_validation_tree("validation-smat.root", simtracks, smat_tracks);
   make_validation_tree("validation-plex.root", simtracks, plex_tracks);
 #endif
+}
+
+//==============================================================================
+
+int main()
+{
+  test_standard();
+
+  // test_matriplex();
 
   return 0;
 }
