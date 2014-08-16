7d06eaa617395cf7e454a30e4f884f15b77269c7
Matevz Tadel <mtadel@ucsd.edu>
2014-08-15 23:04:54 -0700

    Dump output to multify.ah and multify_intr.ah.

--- Matriplex/GMtest.pl
+++ Matriplex/GMtest.pl
@@ -4,16 +4,24 @@ use lib ".";
 
 use GenMul;
 
+### If you're going to run GMtest.cxx and you do some changes here
+### you *MUST* bring DIM, DOM and pattern assumptions in sync!
+
 my $DIM = 3;
 my $DOM = 6;
 
 $a = new GenMul::MatrixSym('name'=>'a', 'M'=>$DIM);
+$a->set_pattern(<<"FNORD");
+x
+x 1 
+x x x
+FNORD
 
 $b = new GenMul::Matrix('name'=>'b', 'M'=>$DIM, 'N'=>$DOM);
 $b->set_pattern(<<"FNORD");
-1 0 0 x 0 0
-0 1 0 0 x 0
-0 0 1 0 0 x
+x x x x 0 x
+x 1 x 1 0 x
+x x x x 0 x
 FNORD
 
 $c = new GenMul::Matrix('name'=>'c', 'M'=>$DIM, 'N'=>$DOM);
@@ -22,9 +30,19 @@ $c = new GenMul::Matrix('name'=>'c', 'M'=>$DIM, 'N'=>$DOM);
 
 $m = new GenMul::Multiply;
 
+
+open STD, ">multify.ah";
+select STD;
+
 $m->multiply_standard($a, $b, $c);
 
-print "\n", '-' x 80, "\n\n";
+close STD;
 
-# Intrinsics do not support patterns yet!
+# print "\n", '-' x 80, "\n\n";
+
+open INT, ">multify_intr.ah";
+select INT;
+
 $m->multiply_intrinsic($a, $b, $c);
+
+close INT;
