259b013d5ed6eea680bebe30dba6020a956784c6
Matevz Tadel <mtadel@ucsd.edu>
2014-08-18 21:18:24 -0700

    Add MultifyTranspose (BT * A = C) and compare values with what's produced by SMatrix.

--- Matriplex/GMtest.cxx
+++ Matriplex/GMtest.cxx
@@ -27,44 +27,48 @@ const float CMP_EPS = 4e-7;
 #endif
 
 typedef ROOT::Math::SMatrix<float, DIM, DOM>                                     SMatX;
+typedef ROOT::Math::SMatrix<float, DOM, DIM>                                     SMatXT;
 typedef ROOT::Math::SMatrix<float, DIM, DIM, ROOT::Math::MatRepSym<float, DIM> > SMatS;
 
 typedef Matriplex::Matriplex   <float, DIM, DOM, N>   MPlexX;
+typedef Matriplex::Matriplex   <float, DOM, DIM, N>   MPlexXT;
 typedef Matriplex::MatriplexSym<float, DIM,      N>   MPlexS;
 
 void Multify(const MPlexS& A, const MPlexX& B, MPlexX& C)
 {
-  typedef float T;
+   // C = A * B
+
+   typedef float T;
 
    const T *a = A.fArray; __assume_aligned(a, 64);
    const T *b = B.fArray; __assume_aligned(b, 64);
          T *c = C.fArray; __assume_aligned(c, 64);
 
-#ifdef MIC_INTRINSICS
-
-   for (int n = 0; n < N; n += 64 / sizeof(T))
-   {
-#include "multify_intr.ah"
-   }
-
-#else
-
-#pragma simd
-   for (int n = 0; n < N; ++n)
-   {
 #include "multify.ah"
-   }
+}
 
-#endif
+void MultifyTranspose(const MPlexS& A, const MPlexX& B, MPlexXT& C)
+{
+   // C = BT * A;
+
+   typedef float T;
+
+   const T *a = A.fArray; __assume_aligned(a, 64);
+   const T *b = B.fArray; __assume_aligned(b, 64);
+         T *c = C.fArray; __assume_aligned(c, 64);
+
+#include "multify-transpose.ah"
 }
 
 int main()
 {
-  SMatS  a[N];
-  SMatX  b[N], c[N];
+  SMatS   a[N];
+  SMatX   b[N],  c[N];
+  SMatXT  bt[N], ct[N];
 
-  MPlexS A;
-  MPlexX B, C;
+  MPlexS  A;
+  MPlexX  B, C;
+  MPlexXT CT;
 
   std::default_random_engine      gen(0xbeef0133);
   std::normal_distribution<float> dis(1.0, 0.05);
@@ -96,10 +100,14 @@ init:
     A.CopyIn(m, a[m].Array());
     B.CopyIn(m, b[m].Array());
 
-    c[m] = a[m] * b[m];
+    c[m]  = a[m] * b[m];
+
+    bt[m] = ROOT::Math::Transpose(b[m]);
+    ct[m] = bt[m] * a[m];
   }
 
   Multify(A, B, C);
+  MultifyTranspose(A, B, CT);
 
   for (int m = 0; m < N; ++m)
   {
@@ -118,7 +126,7 @@ init:
         if (std::abs(c[m](j,k) - C.At(m, j, k)) > CMP_EPS)
         {
           dump = true;
-          printf("M=%d  %d,%d d=%e (count = %lld)\n", m, j, k, c[m](j,k) - C.At(m, j, k), count);
+          printf("MULTIFY   M=%d  %d,%d d=%e (count = %lld)\n", m, j, k, c[m](j,k) - C.At(m, j, k), count);
         }
       }
     }
@@ -147,6 +155,57 @@ init:
       printf("\n");
     }
   }
+
+  // Shameless cut-n-paste of above dump for transpose check with minor changes.
+  // Should make a function, I know ... but ... no time to lose.
+
+  for (int m = 0; m < N; ++m)
+  {
+    bool dump = false;
+
+    for (int j = 0; j < DOM; ++j)
+    {
+      for (int k = 0; k < DIM; ++k)
+      {
+        // There are occasional diffs up to 4.768372e-07 on host, very very
+        // rarely on MIC. Apparently this is a rounding difference between AVX
+        // and normal maths. On MIC it might be usage of FMA?
+        // The above was for 3x3.
+        // For 6x6 practically all elements differ by 4.768372e-07, some
+        // by 9.536743e-07.
+        if (std::abs(ct[m](j,k) - CT.At(m, j, k)) > CMP_EPS)
+        {
+          dump = true;
+          printf("TRANSPOSE M=%d  %d,%d d=%e (count = %lld)\n", m, j, k, ct[m](j,k) - CT.At(m, j, k), count);
+        }
+      }
+    }
+
+    if (dump && false)
+    {
+      printf("\n");
+      for (int i = 0; i < DOM; ++i)
+      {
+        for (int j = 0; j < DIM; ++j)
+          printf("%8f ", ct[m](i,j));
+        printf("\n");
+      }
+      printf("\n");
+
+      for (int i = 0; i < DIM; ++i)
+      {
+        for (int j = 0; j < DOM; ++j)
+          printf("%8f ", CT.At(m, i, j));
+        printf("\n");
+      }
+      printf("\n");
+    }
+    if (dump)
+    {
+      printf("\n");
+    }
+  }
+
 
   ++count;
   goto init;
